c2735069122f9b9b1ed024de592ab6a59ac90ab8
Alex Coronell <alexcoronell@gmail.com>
2021-03-04 11:58:03 -0500

    Reinstalación de Angular

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# Sicoain
+# Sicoainangular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.1.
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
