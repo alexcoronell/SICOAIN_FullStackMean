05c13c0d1513fde9e097a6afffe7ebe3b97dd17e
Alex Coronell <alexcoronell@gmail.com>
2021-03-05 18:11:53 -0500

    Creación de ruta inicial y ajustes en login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userLogin = {
+    user: '',
+    password: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  login() {
+    console.log(this.userLogin);
+  }
+
+  clearData() {
+    this.userLogin.user = '';
+    this.userLogin.password = '';
   }
 
 }
