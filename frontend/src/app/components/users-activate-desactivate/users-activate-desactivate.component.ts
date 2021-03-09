d22ba3a093912fdd9c942f20bbae60c488173dc8
Alex Coronell <alexcoronell@gmail.com>
2021-03-09 17:51:17 -0500

    Maquetación y funcionalidad básica activar desactivar usuarios

--- frontend/src/app/components/users-activate-desactivate/users-activate-desactivate.component.ts
+++ frontend/src/app/components/users-activate-desactivate/users-activate-desactivate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-users-activate-desactivate',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersActivateDesactivateComponent implements OnInit {
 
+  showForm: boolean = false;
+  showSearchForm: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  searchUser() {
+    console.log('Funciono');
+    this.showForm = true;
+    this.showSearchForm = false;
+  }
+
+  actDesactUser(form: NgForm){
+    console.log(form);
+    this.showForm = false;
+    this.showSearchForm = true;
+  }
+
+  clearData(form?: NgForm) {
+    if (form) {
+      form.reset();
+    }
+    this.showForm = false;
+    this.showSearchForm = true;
+  }
 }
