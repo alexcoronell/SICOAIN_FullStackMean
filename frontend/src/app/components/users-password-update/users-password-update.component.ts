9ccd5ff7cfb639f35de9fa2d9ffde5792166813a
Alex Coronell <alexcoronell@gmail.com>
2021-03-09 17:42:20 -0500

    Maquetación y funcionalidad básica update password users

--- frontend/src/app/components/users-password-update/users-password-update.component.ts
+++ frontend/src/app/components/users-password-update/users-password-update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-users-password-update',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersPasswordUpdateComponent implements OnInit {
 
+  showForm: boolean = false;
+  showSearchForm: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  searchUser() {
+    console.log('Funciono');
+    this.showForm = true;
+    this.showSearchForm = false;
+  }
+
+  updatePasswordUser(form: NgForm){
+    console.log(form);
+    this.showForm = false;
+    this.showSearchForm = true;
+  }
+
+  clearData(form?: NgForm) {
+    if (form) {
+      form.reset();
+    }
+    this.showForm = false;
+    this.showSearchForm = true;
   }
 
 }
