6785b15da2baf6d2beaf13a7e8b542322add80f6
Alex Coronell <alexcoronell@gmail.com>
2021-03-10 11:23:32 -0500

    Maquetación de activación/desactivación de empleados

--- frontend/src/app/components/employees-activate-desactivate/employees-activate-desactivate.component.ts
+++ frontend/src/app/components/employees-activate-desactivate/employees-activate-desactivate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Employees } from '../../models/employees';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-employees-activate-desactivate',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesActivateDesactivateComponent implements OnInit {
 
-  constructor() { }
+  employee: Employees;
+
+  showForm: boolean = false;
+  showSearchForm: boolean = true;
+
+  constructor() {
+    this.employee = new Employees;
+  }
 
   ngOnInit(): void {
+  }
+
+  searchEmployee() {
+    console.log('Funciono');
+    this.showForm = true;
+    this.showSearchForm = false;
+  }
+
+  activateDesactivate(form: NgForm){
+    console.log(form);
+    this.showForm = false;
+    this.showSearchForm = true;
+  }
+
+  clearData(form?: NgForm) {
+    if (form) {
+      form.reset();
+    }
+    this.showForm = false;
+    this.showSearchForm = true;
   }
 
 }
