26e47599d94a06f923141332d1255cba17e58848
Alex Coronell <alexcoronell@gmail.com>
2021-03-11 10:21:04 -0500

    Se crean modelos para Registro de incidentes y sucesos

--- frontend/src/app/models/events.ts
+++ frontend/src/app/models/events.ts
@@ -1,2 +1,16 @@
 export class Events {
+
+    constructor(
+        _id = '',
+        name = '',
+        condition = true
+    ){
+        this._id = _id
+        this.name = name,
+        this.condition = condition
+    }
+    
+    _id: string;
+    name: String;
+    condition: Boolean;
 }
--- frontend/src/app/models/records.ts
+++ frontend/src/app/models/records.ts
@@ -1,2 +1,38 @@
 export class Records {
+
+    constructor(
+        _id = '',
+        idRecord = '',
+        employee = '',
+        events = '',
+        dateRecord = '',
+        dataIncident = '',
+        description = '',
+        digitalEvidence = '',
+        reasonForCancellation = '',
+        condition = true
+    ){
+        this._id = _id
+        this.idRecord = idRecord
+        this.employee = employee,
+        this.events = events,
+        this.dateRecord = dateRecord,
+        this.dataIncident = dataIncident,
+        this.description = description,
+        this.digitalEvidence = digitalEvidence,
+        this.reasonForCancellation = reasonForCancellation,
+        this.condition = condition
+    }
+    
+    _id: string;
+    idRecord: String;
+    employee: String;
+    events: String;
+    dateRecord: String;
+    dataIncident: String;
+    description: String;
+    digitalEvidence: String;
+    reasonForCancellation: String;
+    condition: Boolean;
+    
 }
