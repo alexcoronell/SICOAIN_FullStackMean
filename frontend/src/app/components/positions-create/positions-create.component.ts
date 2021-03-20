460da547687b522e1da7e07899be6278efc65fdc
Alex Coronell <alexcoronell@gmail.com>
2021-03-20 15:56:15 -0500

    Terminado creación de cargos

--- frontend/src/app/components/positions-create/positions-create.component.ts
+++ frontend/src/app/components/positions-create/positions-create.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Positions } from '../../models/positions';
+import { PositionsService } from '../../services/positions.service';
+
+declare var M: any;
 
 @Component({
   selector: 'app-positions-create',
@@ -10,22 +14,46 @@ import { Positions } from '../../models/positions';
 export class PositionsCreateComponent implements OnInit {
 
   position: Positions;
+  errorMessage: boolean = false;
 
-  constructor() {
+  constructor(
+    private positionsService: PositionsService,
+    private router: Router
+  ) {
     this.position = new Positions;
   }
 
   ngOnInit(): void {
   }
 
-  createPosition(form?: NgForm) {
-    alert('Funciono');
-    console.log(form);
+  create(Form: NgForm) {
+    this.positionsService.create(Form.value)
+    .subscribe(
+      res => {
+      this.clearData(Form);
+      M.toast({
+        html: 'Cargo creado satisfactoriamente',
+        displayLength: 1500
+      });
+      setTimeout (() => {
+        this.router.navigate(['/employesAndPositions']);
+    }, 1500);
+    },
+      err => {
+        this.errorMessage = true;
+        setTimeout (() => {
+          this.errorMessage = false;
+      }, 1500);
+        M.toast({
+          html: 'Cargo no se pudo guardar',
+          displayLength: 1500
+        })
+    })
   }
 
-  clearData(form?: NgForm) {
-    if (form) {
-      form.reset();
+  clearData(Form?: NgForm) {
+    if (Form) {
+      Form.reset();
       this.position = new Positions();
     }
   }
--- frontend/src/app/services/positions.service.ts
+++ frontend/src/app/services/positions.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Positions } from '../models/positions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PositionsService {
 
-  constructor() { }
+  searchItem: Positions;
+  position: Positions;
+  newCompany: Positions;
+  positions: Positions[];
+
+  private readonly URL_SERVER = "http://localhost:3000/api/positions/";
+  private readonly URL_NEW = "create";
+  private readonly URL_GET = "getPosition";
+  private readonly URL_GET_ALL = "getPositions";
+  private readonly URL_UPDATE = "update";
+  private readonly URL_ACT_DESACT = "actDesact";
+
+  constructor(private http: HttpClient) { }
+
+
+  create(newPosition: Positions) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_NEW, newPosition);
+  }
+
+  getCompany(searchItem) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_GET, searchItem);
+  }
+
+  update(position) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_UPDATE, position);
+  }
+
+  actDesact(position) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_ACT_DESACT, position);
+  }
+
 }
