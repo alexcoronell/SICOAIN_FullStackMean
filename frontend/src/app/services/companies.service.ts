6b99dae98ff1d1fb043bbef9e3aedaf39d2f5699
Alex Coronell <alexcoronell@gmail.com>
2021-03-19 20:06:34 -0500

    Terminado servicios compañías

--- frontend/src/app/services/companies.service.ts
+++ frontend/src/app/services/companies.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Companies } from '../models/companies';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
 
-  constructor() { }
+  searchItem: Companies;
+  company: Companies;
+  newCompany: Companies;
+  companies: Companies[];
+
+  private readonly URL_SERVER = "http://localhost:3000/api/companies/";
+  private readonly URL_NEW = "create";
+  private readonly URL_GET = "getCompany";
+  private readonly URL_GET_ALL = "getCompanies";
+  private readonly URL_UPDATE = "update";
+  private readonly URL_ACT_DESACT = "actDesact";
+
+  constructor(private http: HttpClient) { }
+
+
+  create(newCompany: Companies) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_NEW, newCompany);
+  }
+
+  getUser(searchItem) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_GET, searchItem);
+  }
+
+  update(company) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_UPDATE, company);
+  }
+
+  actDesact(company) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_ACT_DESACT, company);
+  }
 }
