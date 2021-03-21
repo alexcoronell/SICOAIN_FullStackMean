06198255712a4b75ecc31d0834c2ef069c872e73
Alex Coronell <alexcoronell@gmail.com>
2021-03-20 19:11:00 -0500

    Terminado Servicio EPS

--- frontend/src/app/services/eps.service.ts
+++ frontend/src/app/services/eps.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Eps } from '../models/eps';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EpsService {
 
-  constructor() { }
+  searchItem: Eps;
+  Eps: Eps;
+  newEps: Eps;
+  Epss: Eps[];
+
+  private readonly URL_SERVER = "http://localhost:3000/api/epss/";
+  private readonly URL_NEW = "create";
+  private readonly URL_GET = "getEps";
+  private readonly URL_GET_ALL = "getEpss";
+  private readonly URL_UPDATE = "update";
+  private readonly URL_ACT_DESACT = "actDesact";
+
+  constructor(private http: HttpClient) { }
+
+
+  create(newEps: Eps) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_NEW, newEps);
+  }
+
+  getEps(searchItem) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_GET, searchItem);
+  }
+
+  update(Eps) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_UPDATE, Eps);
+  }
+
+  actDesact(Eps) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_ACT_DESACT, Eps);
+  }
 }
