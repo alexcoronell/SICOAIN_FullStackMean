5e39118a3d2de7b907a76def1bd06bf851424942
Alex Coronell <alexcoronell@gmail.com>
2021-03-22 12:43:54 -0500

    Realizado servicio frontend Empleados

--- frontend/src/app/services/employees.service.ts
+++ frontend/src/app/services/employees.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employees } from '../models/employees';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
 
-  constructor() { }
+  searchItem: Employees;
+  employee: Employees;
+  newEmployee: Employees;
+  employees: Employees[];
+
+  private readonly URL_SERVER = "http://localhost:3000/api/employees/";
+  private readonly URL_NEW = "create";
+  private readonly URL_GET = "getEmployee";
+  private readonly URL_GET_ALL = "getEmployees";
+  private readonly URL_UPDATE = "update";
+  private readonly URL_ACT_DESACT = "actDesact";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getEmployees() {
+    return this.http.get<Employees[]>(this.URL_SERVER + this.URL_GET);
+  }
+
+  create(newEmployee: Employees) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_NEW, newEmployee);
+  }
+
+  getEmployee(searchItem) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_GET, searchItem);
+  }
+
+  update(employee) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_UPDATE, employee);
+  }
+
+  actDesact(employee) {
+    return this.http.post<any>(this.URL_SERVER + this.URL_ACT_DESACT, employee);
+  }
+
 }
