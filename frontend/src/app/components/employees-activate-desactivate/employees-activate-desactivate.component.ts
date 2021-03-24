e499f0f412d5b1aa85e8750410f9705c0391da24
Alex Coronell <alexcoronell@gmail.com>
2021-03-24 13:21:23 -0500

    Terminada activación y desactivación de empleados

--- frontend/src/app/components/employees-activate-desactivate/employees-activate-desactivate.component.ts
+++ frontend/src/app/components/employees-activate-desactivate/employees-activate-desactivate.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Employees } from '../../models/employees';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EmployeesService } from 'src/app/services/employees.service';
+import { Employees } from '../../models/employees';
+
+declare var M: any;
 
 @Component({
   selector: 'app-employees-activate-desactivate',
@@ -10,35 +14,76 @@ import { NgForm } from '@angular/forms';
 export class EmployeesActivateDesactivateComponent implements OnInit {
 
   employee: Employees;
-
+  searchItem: Employees;
+  errorMessage: boolean = false
   showForm: boolean = false;
   showSearchForm: boolean = true;
+  searchValidate: boolean = false;
 
-  constructor() {
+  constructor(
+    private employeesService: EmployeesService,
+    private router: Router
+  ) {
     this.employee = new Employees;
+    this.searchItem = new Employees;
   }
 
   ngOnInit(): void {
   }
 
-  searchEmployee() {
-    console.log('Funciono');
-    this.showForm = true;
-    this.showSearchForm = false;
+  search() {
+    this.employeesService.getEmployee(this.searchItem)
+    .subscribe(
+      res => {
+        this.employee = res.employeeData;
+        this.showForm = true;
+        this.showSearchForm = false;
+      },
+      err => {
+        if (err.error == "The Employee doen't exist") {
+          this.searchValidate = true;
+        setTimeout (() => {
+          this.searchValidate = false;
+      }, 1500);
+        } else {
+          console.log(err.error);
+        }
+          this.clearSearchForm();
+        }
+    )
   }
 
-  activateDesactivate(form: NgForm){
-    console.log(form);
-    this.showForm = false;
-    this.showSearchForm = true;
+  actDesact(Form: NgForm){
+    this.employeesService.actDesact(Form.value)
+    .subscribe(
+      res => {
+        M.toast({
+        html: 'Condición actualizada correctamente',
+        displayLength: 1500
+      });
+        setTimeout (() => {
+        this.router.navigate(['/employesAndPositions']);
+    }, 1500);
+    this.clearData(Form);
+    },
+      err => {
+        console.log(err)
+        M.toast({
+          html: 'Condición no se pudo actualizar',
+          displayLength: 1500
+        })
+    })
   }
 
-  clearData(form?: NgForm) {
-    if (form) {
-      form.reset();
+  clearData(Form?: NgForm) {
+    if (Form) {
+      Form.reset();
+      this.employee = new Employees;
     }
-    this.showForm = false;
-    this.showSearchForm = true;
+  }
+
+  clearSearchForm() {
+    this.searchItem.identificationNumber = "";
   }
 
 }
