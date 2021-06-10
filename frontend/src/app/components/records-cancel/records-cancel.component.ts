dfe73d243901a93f68e0a6517ccf6204ffdafce5
Alex Coronell <alexcoronell@gmail.com>
2021-06-10 08:15:36 -0500

    Se cargan registros en formulario de búsqueda cancelación de registros

--- frontend/src/app/components/records-cancel/records-cancel.component.ts
+++ frontend/src/app/components/records-cancel/records-cancel.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Records } from '../../models/records';
+import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { Records } from '../../models/records';
+import { RecordsService } from './../../services/records.service';
+
+// Servicios y modelos de campos adicionales
+import { EmployeesService } from 'src/app/services/employees.service';
+import { Employees } from './../../models/employees';
+import { Events } from './../../models/events';
+import { EventsService } from './../../services/events.service';
 
 @Component({
   selector: 'app-records-cancel',
@@ -10,21 +18,73 @@ import { NgForm } from '@angular/forms';
 export class RecordsCancelComponent implements OnInit {
 
   record: Records;
+  records: Records[];
+  searchItem: Records;
+  errorMessage:boolean = false;
+  searchValidate: boolean = false;
+
+  fileUpload:any;
+
+  employee: Employees;
+  employees: Employees[];
+  event: Events;
+  events: Events[];
 
   showForm: boolean = false;
   showSearchForm: boolean = true;
 
-  constructor() {
+  constructor(
+    private recordsService: RecordsService,
+    private employeesService: EmployeesService,
+    private eventsService: EventsService,
+    private router: Router
+  ) {
     this.record = new Records;
+    this.employee = new Employees;
+    this.event = new Events;
+    this.searchItem = new Records;
   }
 
   ngOnInit(): void {
+    this.getRecords();
   }
 
-  search() {
-    console.log('Funciono');
-    this.showForm = true;
-    this.showSearchForm = false;
+   // Búsqueda y carga de registro
+   search(SearchForm: NgForm) {
+    this.searchItem.idRecord = this.searchItem.idRecord.trim();
+    this.recordsService.getRecord(this.searchItem)
+    .subscribe(
+      res => {
+        this.record = res.recordData;
+        this.clearSearchForm();
+        this.showForm = true;
+        this.showSearchForm = false;
+      },
+      err => {
+        if (err.error == "The record doen't exist") {
+          this.searchValidate = true;
+        setTimeout (() => {
+          this.searchValidate = false;
+      }, 1500);
+        } else {
+          console.log(err.error);
+        }
+          this.clearSearchForm();
+        }
+    )
+  }
+
+  // Obtener todos los registros
+  getRecords() {
+    this.recordsService.getRecords()
+      .subscribe(
+        res => {
+          this.records = res as Records[];
+        },
+        err => {
+          console.error(err.error);
+        }
+      )
   }
 
   update(form: NgForm) {
@@ -32,11 +92,16 @@ export class RecordsCancelComponent implements OnInit {
     this.clearData();
   }
 
+  clearSearchForm() {
+    this.searchItem.idRecord = "";
+  }
+
   clearData(form?: NgForm) {
     if (form) {
       form.reset();
       this.record = new Records;
     }
+    this.clearSearchForm()
     this.showForm = false;
     this.showSearchForm = true;
   }
