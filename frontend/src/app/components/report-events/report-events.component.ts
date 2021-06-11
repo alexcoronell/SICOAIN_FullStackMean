5b56fed143e8db623e7bf717e713efa16be2bcbf
Alex Coronell <alexcoronell@gmail.com>
2021-06-11 13:18:17 -0500

    Carga de datos en reportes de Sucesos

--- frontend/src/app/components/report-events/report-events.component.ts
+++ frontend/src/app/components/report-events/report-events.component.ts
@@ -1,4 +1,7 @@
+import { EventsService } from './../../services/events.service';
+import { Events } from './../../models/events';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-report-events',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportEventsComponent implements OnInit {
 
-  constructor() { }
+  events: Events[];
+
+  constructor(
+    private eventsService: EventsService
+  ) { }
 
   ngOnInit(): void {
+    this.getEvents();
+  }
+
+  // Obtener todos los sucesos
+  getEvents = () => {
+    this.eventsService.getEvents()
+      .subscribe(
+        res => {
+          this.events = res as Events[];
+        },
+        err => {
+          console.error(err.error);
+        }
+      )
   }
 
 }
